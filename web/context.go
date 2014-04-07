fbc9b879f7880254ca95c7afe6dae0c65867783a
oliver <dumanxiang@gmail.com>
2014-04-07 15:43:57 +0800

    context finished

--- web/context.go
+++ web/context.go
@@ -4,14 +4,31 @@ import (
 	"net/http"
 )
 
+type parameters map[string]interface{}
+
 type context struct {
 	ip             string
 	ResponseWriter http.ResponseWriter
 	Request        *http.Request
+	Params         parameters
 }
 
 func NewContext(rw http.ResponseWriter, req *http.Request) *context {
-	return &context{getIP(req), rw, req}
+	p := params(req)
+	return &context{getIP(req), rw, req, p}
+}
+
+func params(req *http.Request) parameters {
+	req.ParseForm()
+	if len(req.Form) > 0 {
+		p := make(parameters)
+		for k, v := range req.Form {
+			p[k] = v
+		}
+		return p
+	}
+
+	return nil
 }
 
 func (this *context) IP() string {
--- web/router.go
+++ web/router.go
@@ -2,7 +2,26 @@ package web
 
 import (
 	"net/http"
+	"reflect"
 )
 
+var (
+	gControllers = make(map[string]reflect.Value)
+)
+
+func Add(i interface{}) {
+	key := reflect.TypeOf(i).String()
+	val := reflect.ValueOf(i)
+	gControllers[key] = val
+}
+
 func router(rw http.ResponseWriter, req *http.Request) {
+	ctx := NewContext(rw, req)
+	k := ctx.Params["controler"].(string)
+	a := ctx.Params["action"].(string)
+
+	method := gControllers[k+"Controler"].MethodByName(a + "Action")
+	p := make([]reflect.Value, 0, 1)
+	p[0] = reflect.ValueOf(ctx)
+	method.Call(p)
 }
